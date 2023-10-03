46c9b94570f28fae23c35b83a4bec0ecb0dd9cc0
Kingwizard96 <calvarado1996@gmail.com>
2023-10-02 22:53:42 -0500

    still facing error

--- Develop/lib/shapes.js
+++ Develop/lib/shapes.js
@@ -1,27 +1,36 @@
+// Shape class utilizes constructor to define what it means to be a shape
 class Shape {
     constructor() {
-        this.color() = '';
+      this.color = "";
     }
-    setColor(colorVar){
-        this.color = colorVar
+    setColor(colorVar) {
+      this.color = colorVar;
     }
-}
-
-class Triangle extends Shape {
+  }
+  
+  // Triangle class inherits properties defined in Shape class
+  class Triangle extends Shape {
     render() {
-        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+      // Returns polygon with color input
+      return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
     }
-}
- class Square extends Shape {
+  }
+  
+  // Square class inherits properties defined in Shape class
+  class Square extends Shape {
     render() {
-        return `<rect x ="73" y="40" width="160" height="160" fill="${this.color}" />`;
+      // Returns polygon with color input
+      return `<rect x="73" y="40" width="160" height="160" fill="${this.color}" />`;
     }
- }
-
- class Circle extends Shape {
-   render() {
-    return `<circle cx="150" cy="115" r="80" fill="${this.color}" />`;
+  }
+  
+  // Circle class inherits properties defined in Shape class
+  class Circle extends Shape {
+    render() {
+      // Returns polygon with color input
+      return `<circle cx="150" cy="115" r="80" fill="${this.color}" />`;
     }
- }
-
- module.exports = { Triangle,Square, Circle};
+  }
+  
+  // Exports classes (Square, Triangle, Circle)
+  module.exports = { Triangle, Square, Circle };
--- Develop/lib/shapes.test.js
+++ Develop/lib/shapes.test.js
@@ -1,28 +1,35 @@
-const {Triangle, Square, Circle} = require("./shapes.js");
+const { Triangle, Square, Circle } = require("./shapes.js");
+
+// write a test
 
 describe("Triangle test", () => {
-    test("test for a triangle with a background color", () => {
-    const shape = new Triangle ();
-    shape.setColor('red');
-    expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56,182" fill="red" />'
-        );
-    });
+  test("test for a triangle with a red background", () => {
+    const shape = new Triangle();
+    shape.setColor("red");
+    expect(shape.render()).toEqual(
+      '<polygon points="150, 18 244, 182 56, 182" fill="red" />'
+    );
+  });
 });
+
 
 describe("Square test", () => {
-    test("test for a square with a background color", () => {
-    const shape = new Square ();
-    shape.setColor('blue');
-    expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56,182" fill="blue" />'
-        );
-    });
+  test("test for a square with a blue background", () => {
+    const shape = new Square();
+    shape.setColor("blue");
+    expect(shape.render()).toEqual(
+      '<rect x="73" y="40" width="160" height="160" fill="blue" />'
+    );
+  });
 });
 
+
 describe("Circle test", () => {
-    test("test for a circle with a background color", () => {
-    const shape = new Circle ();
-    shape.setColor('purple');
-    expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56,182" fill="purple" />'
-        );
-    });
+  test("test for a circle with a # background", () => {
+    const shape = new Circle();
+    shape.setColor("#333333");
+    expect(shape.render()).toEqual(
+      '<circle cx="150" cy="115" r="80" fill="#333333" />'
+    );
+  });
 });
