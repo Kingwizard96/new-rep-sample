eacebaa2562123a1d4cee5f5bb1521165674a2d8
Kingwizard96 <calvarado1996@gmail.com>
2023-10-02 19:29:11 -0500

    wrote code in shapes.js

--- Develop/lib/shapes.js
+++ Develop/lib/shapes.js
@@ -1,71 +1,27 @@
-const inquirer = require('inquirer');
-const { join }  = require('path');
-const { writeFile } = require('fs/promises');
-const { createDocument } = require('./document');
-
-class CLI {
-    constructor(){
-        this.title = '';
-
-        // array of task object e.g [{text: string,priority: bool},...]
-        this.task = [];
+class Shape {
+    constructor() {
+        this.color() = '';
     }
-    run() {
-      return inquirer 
-        .prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'enter your name',
-            },
-        ])
-            .then(({ name }) => {
-                this.title = `${name}'s tasks`;
-                return this.addTask();
-            })
-            .then(() => {
-                //  sort by priorty
-                this.task.sort((a,b)=> 
-                a.priority === b.priority ? 0 : a.priority  && b.priority ? -1 : 1
-            );
-            return writeFile(
-                join(_dirname, '..', 'output', 'tasks.html'), 
-                createDocument(this.title, this.tasks)
-            );
-        })
-        .then(() => console.log('created tasks.html'))
-        .catch((err) => {
-            console.log(err);
-            console.log('oops.something went wrong');
-            });
-    } 
-    
-    addTask() {
-        return inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'text',
-                message: 'enter task',
-            },
-            {
-                type: 'confirm',
-                name: 'priority',
-                message: 'is this a priority task?',
-            },
-            {
-                type: 'confirm',
-                name: 'confirmAddTask',
-                message: 'would you like to add another task',
-            },
-        ])
-        .then(({ text, priority, confrimAddTask }) => {
-            this.task.push({text, priority });
-            if (confrimAddTask) {
-                return this.addTask();
-            }
-        });
+    setColor(colorVar){
+        this.color = colorVar
     }
 }
 
-module.exports = CLI;
+class Triangle extends Shape {
+    render() {
+        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+    }
+}
+ class Square extends Shape {
+    render() {
+        return `<rect x ="73" y="40" width="160" height="160" fill="${this.color}" />`;
+    }
+ }
+
+ class Circle extends Shape {
+   render() {
+    return `<circle cx="150" cy="115" r="80" fill="${this.color}" />`;
+    }
+ }
+
+ module.exports = { Triangle,Square, Circle};
